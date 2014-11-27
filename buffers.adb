26d0df38950a59be074d5dff472338b78209d1fc
rybycy <michal.robaszynski@gmail.com>
2014-11-27 10:54:58 +0100

    debugged

--- buffers.adb
+++ buffers.adb
@@ -6,25 +6,27 @@ package body Buffers is
       Actual_Size : Integer := 1;
       Data : Array(0..Size-1) of Integer;
       Test : Boolean := False ;
+
    begin
       loop
          select
-            when ((Size > 1 and Start /= Actual_Size) or (Size = 1 and not Test)) =>
-            accept Put ( Obj : in Integer ) do
+            --when ((Size > 1 and Start /= Actual_Size) or (Size = 1 and not Test)) =>
+            when (Actual_Size /= Start) =>
+               accept Put ( Obj : in Integer ) do
 
-               Data( Actual_Size - 1 ) := Obj;
-               Actual_Size := (Actual_Size + 1 ) mod Size;
-               Test := True;
+                  Data( (Actual_Size - 1) mod Size ) := Obj;
+                  Actual_Size := (Actual_Size + 1 ) mod Size;
+                  Test := True;
 
-            end Put;
+               end Put;
          or
-            when ((Size > 1 and (Start /= ((Actual_Size - 1) mod Size))) or (Size = 1 and Test)) =>
-            accept Get (Obj : out Integer) do
-               Obj := Data ( Start );
-               Start := (Start + 1) mod Size;
-               Test := False;
-
-            end Get;
+              --when ((Size > 1 and (Start /= ((Actual_Size - 1) mod Size))) or (Size = 1 and Test)) =>
+            when (Start /= ((Actual_Size - 1) mod Size)) =>
+               accept Get (Obj : out Integer) do
+                  Obj := Data ( Start );
+                  Start := (Start + 1) mod Size;
+                  Test := False;
+               end Get;
          or
             terminate;
          end select;
